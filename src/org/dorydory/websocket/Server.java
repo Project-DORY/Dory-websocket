566c11a6cab4c9be9864ac87ab92aaa9d4bd5a13
bethesirius <hs4393@gmail.com>
2015-10-13 19:35:53 +0900

    echo server

--- src/org/dorydory/websocket/Server.java
+++ src/org/dorydory/websocket/Server.java
@@ -1,21 +1,34 @@
 package org.dorydory.websocket;
 
+import io.vertx.core.buffer.Buffer;
 import io.vertx.core.AbstractVerticle;
+import io.vertx.core.Handler;
+import io.vertx.core.http.ServerWebSocket;
 import org.dorydory.websocket.util.Runner;
+import io.vertx.core.logging.Logger;
 
-/*
- * @author <a href="http://tfox.org">Tim Fox</a>
- */
 public class Server extends AbstractVerticle {
 
-    // Convenience method so you can run it in your IDE
     public static void main(String[] args) {
         Runner.runExample(Server.class);
     }
 
     @Override
     public void start() throws Exception {
-        vertx.createHttpServer().websocketHandler(ws -> ws.handler(ws::writeBinaryMessage)).requestHandler(req ->
-                req.response().end("Hello World!")).listen(8080);
+
+        vertx.createHttpServer().websocketHandler(new Handler<ServerWebSocket>() {
+            @Override
+            public void handle(final ServerWebSocket ws) {
+                ws.writeFinalTextFrame("Connected Succesfully!");
+
+                ws.handler(new Handler<Buffer>() {
+                    @Override
+                    public void handle(final Buffer data) {
+                        ws.writeFinalTextFrame(data.toString());
+                    }
+                });
+
+            }
+        }).listen(8080);
     }
 }
